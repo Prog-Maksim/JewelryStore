4e4042e909932414d5d6985ff619b31d26d57799
maksim beloglazov <maksimbel20170@gmail.com>
2025-02-12 12:48:29 +0300

    Добавление адресов для пользователей

---
+++ JewelryStoreBackend/Models/AddressModel/Address.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace JewelryStoreBackend.Models.AddressModel;
+
+public class Address
+{
+    public string house_number { get; set; }
+    public string road { get; set; }
+    public string suburb { get; set; }
+    public string city_district { get; set; }
+    public string city { get; set; }
+    public string county { get; set; }
+    public string state { get; set; }
+    
+    [JsonProperty("ISO3166-2-lvl4")]
+    public string ISO31662lvl4 { get; set; }
+    public string postcode { get; set; }
+    public string country { get; set; }
+    public string country_code { get; set; }
+}
---
+++ JewelryStoreBackend/Models/DB/User/Address.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JewelryStoreBackend.Models.DB.User;
+
+public class Address
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Идентификатор адреса
+    /// </summary>
+    public string AddressId { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public string PersonId { get; set; }
+    
+    /// <summary>
+    /// Страна
+    /// </summary>
+    public string Country { get; set; }
+    
+    /// <summary>
+    /// Город
+    /// </summary>
+    public string City { get; set; }
+    
+    /// <summary>
+    /// Адрес
+    /// </summary>
+    public string AddressLine1 { get; set; }
+    
+    /// <summary>
+    /// Дополнительный адрес
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? AddressLine2 { get; set; }
+    
+    /// <summary>
+    /// Почтовый индекс
+    /// </summary>
+    public string? PostalCode { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public DateTime CreateAt { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public DateTime? UpdateAt { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public string lat { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public string lon { get; set; }
+}
+
