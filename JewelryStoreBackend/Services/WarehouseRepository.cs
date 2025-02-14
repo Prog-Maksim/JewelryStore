19b634cfd2378eefa96c135864d53d46052ca7db
maksim beloglazov <maksimbel20170@gmail.com>
2025-02-14 13:41:53 +0300

    дополнительные сервисы

---
+++ JewelryStoreBackend/Services/WarehouseRepository.cs
@@ -0,0 +1,20 @@
+﻿using JewelryStoreBackend.Models.DB.Product;
+using MongoDB.Driver;
+
+namespace JewelryStoreBackend.Services;
+
+public class WarehouseRepository
+{
+    private readonly IMongoCollection<Warehouse> _warehousesCollection;
+
+    public WarehouseRepository(IMongoClient mongoClient)
+    {
+        var database = mongoClient.GetDatabase("JewelryStoreDB");
+        _warehousesCollection = database.GetCollection<Warehouse>("Warehouses");
+    }
+    
+    public async Task AddMessageAsync(Warehouse product)
+    {
+        await _warehousesCollection.InsertOneAsync(product);
+    }
+}
